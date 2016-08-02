cd86a24e157c7a58e4027c21b64a118eb8d84c2d
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-02 12:55:52 -0500

    Date Objects

--- app.js
+++ app.js
@@ -6,3 +6,22 @@ document.write(Math.PI);
 var n = prompt("Enter a number", "");
 var answer = Math.sqrt(n);
 alert("The square root of " + n + " is " + answer);
+
+/****************************************************
+*                    Date Objects                   *
+****************************************************/
+function doSomething(){
+  document.write("Tuna ");
+}
+
+//setInterval("doSomething", 1000);
+
+function printTime(){
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  document.write(hours + ":" + minutes + ":" + seconds + "<br>");
+}
+
+printTime();
